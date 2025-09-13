be855c4c870fc748d782c3a552ec5115692113ba
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-09-13 15:24:08 +0600

    added

--- src/app/dashboard/patient/doctor-profile/page.jsx
+++ src/app/dashboard/patient/doctor-profile/page.jsx
@@ -1,14 +1,25 @@
-"use client";
+// "use client";
 
-import { doctors } from "../data/doctor";
-import DoctorCard from "./components/doctorCard";
+// import { doctors } from "../data/doctor";
+// import DoctorCard from "./components/doctorCard";
 
-export default function DoctorProfilePage() {
+// export default function DoctorProfilePage() {
+//   return (
+//     <main className="min-h-screen bg-gray-100 flex flex-col items-center p-6 space-y-4">
+//       {doctors.map((doctor) => (
+//         <DoctorCard key={doctor.id} doctor={doctor} />
+//       ))}
+//     </main>
+//   );
+// }
+import React from 'react';
+
+const page = () => {
   return (
-    <main className="min-h-screen bg-gray-100 flex flex-col items-center p-6 space-y-4">
-      {doctors.map((doctor) => (
-        <DoctorCard key={doctor.id} doctor={doctor} />
-      ))}
-    </main>
+    <div>
+
+    </div>
   );
-}
+};
+
+export default page;
